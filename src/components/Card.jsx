4d32c266bda7da94b34a5e0ad6e6815b5f8c3dfc
dariomicelii <dariomiceli01@gmail.com>
2024-12-02 22:55:08 +0100

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,19 @@
 import "../data/languages";
 
-export default function Card({ title, description }) {
+export default function Card({
+  id,
+  index,
+  title,
+  description,
+  isOpen,
+  handleStatusChange,
+}) {
+  const openButton = isOpen ? "open" : "";
   return (
-    <div className="card p-3 my-5">
+    <div
+      className={"card p-3 my-5 d-none" + openButton}
+      onClick={() => handleStatusChange(index)}
+    >
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
